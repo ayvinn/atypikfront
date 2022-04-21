49c59fa74fcd5ca39281920c25e9319ad088ff69
Ayoub EL HAFI <ucseme@gmail.com>
2022-04-21 21:32:25 +0000

    Afficher les accomodations sur la  home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,7 @@ import { filter, map } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
 import { MediaChange, MediaObserver } from '@angular/flex-layout'; 
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators'; 
+import { AccommodationsService } from 'src/app/services/accommodations.service';
 
 @Component({
   selector: 'app-home',
@@ -19,7 +20,7 @@ export class HomeComponent implements OnInit {
 
   public slides = [];
   public test:string = "dfsdfsdfsdf";
-  public properties: Property[];
+  public properties: any[] = [];
   public viewType: string = 'grid';
   public viewCol: number = 25;
   public count: number = 8;
@@ -28,11 +29,11 @@ export class HomeComponent implements OnInit {
   public removedSearchField: string;
   public pagination:Pagination = new Pagination(1, 8, null, 2, 0, 0); 
   public message:string;
-  public featuredProperties: Property[];
-  public locations: Location[]; 
+  public featuredProperties: Property[] = [];
+  public locations: Location[] =[]; 
 
   public settings: Settings;
-  constructor(public appSettings:AppSettings, public appService:AppService, public mediaObserver: MediaObserver) {
+  constructor(public appSettings:AppSettings, public appService:AppService, public mediaObserver: MediaObserver, public accomodationservice : AccommodationsService) {
     this.settings = this.appSettings.settings;
 
     this.watcher = mediaObserver.asObservable()
@@ -90,10 +91,10 @@ export class HomeComponent implements OnInit {
   public getProperties(){  
     //console.log('get properties by : ', this.searchFields);  
     this.appService.getProperties().subscribe(data => {      
-      if(this.properties && this.properties.length > 0){  
+      /*if(this.properties && this.properties.length > 0){  
         this.settings.loadMore.page++;
         this.pagination.page = this.settings.loadMore.page; 
-      }
+      }*/
       let result = this.filterData(data); 
       if(result.data.length == 0){
         this.properties.length = 0;
@@ -105,7 +106,45 @@ export class HomeComponent implements OnInit {
         this.properties = this.properties.concat(result.data);          
       }
       else{
-        this.properties = result.data;  
+        this.accomodationservice.getAccommodations({page:0}).subscribe(res  =>{
+            console.log(res);
+            res['accommodations'].forEach(element => {
+              console.log(element.title);
+              var photos = [];
+              element.photos.forEach(photo => {
+                    photos.push({small : photo.name,big:photo.name,medium:photo.name});
+              });
+              const values = {title : element.title,bathrooms: element.bathrooms,bedrooms:element.bedrooms,city:element.address.city,
+                                additionalFeatures: [],
+                                area: {value: element.travelers, unit: "voyageur"},
+                                desc: element.description,
+                                featured: false,
+                                features: ["Climatisation", "Barbeque", "Séchoir", "Micro-ondes", "Réfrigérateur", "Cheminée", "Piscine"],
+                                formattedAddress: element.address.street+', '+element.address.city+', '+element.address.postalCode,
+                                gallery: photos,
+                                garages: element.beds,
+                                id: element.id,
+                                lastUpdate: "2019-05-20 14:20:00",
+                                location: {lat: 40.84915, lng: -73.9351},
+                                neighborhood: ["Astoria", "Midtown"],
+                                plans: [{name: "First floor"}, {name: "Second floor"}],
+                                priceDollar: {sale: 1300000, rent: null},
+                                priceEuro: {sale: element.price, rent: null},
+                                propertyStatus: ["Disponible"],
+                                propertyType: element.kind,
+                                published: element.user.dateRegistration,
+                                ratingsCount: 1,
+                                ratingsValue: element.environmentalAverage,
+                                street: [element.address.street],
+                                videos: [],
+                                views: 322,
+                                yearBuilt: 2007,
+                                zipCode: element.address.postalCode,
+              }
+              this.properties.push(values);
+            });
+        })
+       // this.properties = result.data;  
       } 
       this.pagination = result.pagination;
       this.message = null;
